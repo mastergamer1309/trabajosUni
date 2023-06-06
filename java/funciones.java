204d4b77c52b81b59a84973ffb2c53a220c3acd2
mastergamer1309 <josiaszeledon20@gmail.com>
2023-06-05 21:11:17 -0600

    asd

---
+++ java/funciones.java
@@ -0,0 +1,17 @@
+public class funciones {
+    public static void main(String []args){
+        PrintName("josias");
+        int suma = sum(4,5);
+        System.out.println(suma);
+    }
+        static void PrintName(String name){
+            System.out.println("hola " + name);
+    }
+        
+        static int sum(int n1, int n2){
+            return n1+n2;            
+
+        }
+
+    }
+
