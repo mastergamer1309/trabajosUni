f4cc416f8ec1f403a3cc4c96144916369838d680
mastergamer1309 <josiaszeledon20@gmail.com>
2023-06-16 17:21:40 -0600

    asd

---
+++ EstudioExamenProfeJuan/insertionSort.java
@@ -0,0 +1,14 @@
+package EstudioExamenProfeJuan;
+import java.util.Scanner;
+public class insertionSort {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ingrese el numero de elementos del arreglo: ");
+        int numeroElementos = scanner.nextInt();
+
+        
+    }
+    
+
+
+}
