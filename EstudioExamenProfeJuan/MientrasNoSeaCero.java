cac77eafbd103c569a589f5e0f79b14490b320a4
mastergamer1309 <josiaszeledon20@gmail.com>
2023-06-17 17:17:36 -0600

    a

---
+++ EstudioExamenProfeJuan/MientrasNoSeaCero.java
@@ -0,0 +1,19 @@
+package EstudioExamenProfeJuan;
+import java.util.Scanner;
+public class MientrasNoSeaCero {
+    public static void main(String []args){
+        Scanner scanner = new Scanner(System.in);
+        int suma = 0;
+        int num = 0;
+
+
+        do{
+            System.out.println("ingrese un numero: ");
+            num = scanner.nextInt();
+            suma += num;
+        }while(num !=0);
+
+        System.out.println(suma);
+    }
+    
+}
